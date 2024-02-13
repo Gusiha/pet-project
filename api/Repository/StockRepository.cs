8fc8fdb22dd0faab82b6ed7f0beebd168a54355a
Gusiha <TUGARIN@TUGARIN>
2024-02-13 19:55:44 +0300

    feat : add paging for stocks

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -63,8 +63,11 @@ namespace api.Repository
                 }
             }
 
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-            return await stocks.ToListAsync();
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 
         }
 
